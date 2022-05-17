8f893400f86327fc5ccab91dd96605590c24a2d6
Scube Scripts <104854776+ScubeScripts@users.noreply.github.com>
2022-05-17 11:14:50 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'bodacious'
 game 'gta5'
 
-name "bb_phonetax"
-author "BigBull Scripts"
+name "sc_phonetax"
+author "Scube Scripts"
 version "1.0.1"
 description "Lets players pay a phone bill"
   
